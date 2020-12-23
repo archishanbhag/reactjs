eff47fd57b6c03576f6a823eebf57a181a7be453
archishanbhag <76509049+archishanbhag@users.noreply.github.com>
2020-12-23 11:28:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 const items = [
- { number: "1", title: "🇦🇷 Argentina"},
- { number: "2", title: "🤩 YASS"},
- { number: "3", title: "👩🏼‍💻 Tech Girl"},
- { number: "4", title: "💋 Lipstick & Code"},
- { number: "5", title: "💃🏼 Latina"},
+ { number: "1", title: "Argentina"},
+ { number: "2", title: "YASS"},
+ { number: "3", title: "Tech Girl"},
+ { number: "4", title: " Lipstick & Code"},
+ { number: "5", title: "Latina"},
 ]
 
 const initialDnDState = {
